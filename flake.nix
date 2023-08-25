000b849afcfbb08044ecf1cb57e1db14872c587c
Yaroslav Bolyukin <iam@lach.pw>
2023-08-25 10:24:55 +0200

    ci: init nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Baedeker - Substrate chain testing framework";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust = ((pkgs.rustChannelOf { date = "2023-08-02"; channel = "nightly"; }).default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+          targets = ["x86_64-unknown-linux-musl" "x86_64-unknown-linux-gnu"];
+        });
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;[
+            rust
+            cargo-edit
+          ];
+        };
+      }
+    );
+}
