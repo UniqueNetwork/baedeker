53f73774aa30c2d15595ab72134017bfe4c93824
Yaroslav Bolyukin <iam@lach.pw>
2024-05-24 01:48:33 +0200

    build: update chainql

--- flake.nix
+++ flake.nix
@@ -3,26 +3,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+          overlays = [rust-overlay.overlays.default];
         };
-        rust = ((pkgs.rustChannelOf { date = "2023-08-02"; channel = "nightly"; }).default.override {
-          extensions = [ "rust-src" "rust-analyzer" ];
-          targets = ["x86_64-unknown-linux-musl" "x86_64-unknown-linux-gnu"];
-        });
-      in
-      rec {
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs;[
+          nativeBuildInputs = with pkgs; [
             rust
             cargo-edit
+            rustPlatform.bindgenHook
           ];
+
+          PROTOC = "${pkgs.protobuf}/bin/protoc";
         };
       }
     );
